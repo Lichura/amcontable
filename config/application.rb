f0d41381e5dd8fe52946d3f4398659749f41a932
Lisandro Gurman <lichun88@gmail.com>
2015-12-31 17:28:30 -0300

    prueba

--- config/application.rb
+++ config/application.rb
@@ -3,8 +3,7 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
-config.assets.paths << Rails.root.join("app", "assets", "fonts")
-config.assets.precompile += %w(.svg .eot .woff .ttf)
+
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -26,5 +25,8 @@ module Mbcontable
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    config.assets.paths << Rails.root.join("app", "assets", "fonts")
+    config.assets.precompile += %w(.svg .eot .woff .ttf)
   end
 end
